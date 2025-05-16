b53796288c30f4a56db3fc9f42e60e76e770f0fa
MartinMueller2003 <MartinMueller2003@users.noreply.github.com>
2025-05-16 22:22:44 +0000

    deploy: ce0348436cf63b701f8b07fb938b300ca625b182

--- doxy/navtreedata.js
+++ doxy/navtreedata.js
@@ -56,19 +56,19 @@ var NAVTREEINDEX =
 "_const_names_8cpp.html",
 "_const_names_8hpp.html#a0ab00f77a5cb19f5db48543a0f880853",
 "_e_s_pixel_stick_8h.html#a067b67c856af5452506085e83cdb2b01",
-"_g_p_i_o___defs___e_s_p32___d1___m_i_n_i___e_t_h_8hpp.html#aca6c98b14a61849654e93ee35fd1d1ab",
-"_g_p_i_o___defs___e_s_p32___m_h___e_t___l_i_v_e___mini_kit_8hpp.html#a3e89ca4ee0a6efe86f854e9f51d8c8e7",
-"_g_p_i_o___defs___e_s_p32___q_u_i_n_l_e_d___q_u_a_d___e_t_h_8hpp.html#ad99563c913b8da40ad1a5a305bf3ed3d",
-"_g_p_i_o___defs___e_s_p32___t_w_i_l_i_g_h_t_l_o_r_d___e_t_h_8hpp.html#a6935dea675e388d8e9c6fe125d36c4e6",
-"_input_d_d_p_8h.html#ad8fce9f74a6e8c99bcb25bdff26fe972",
-"_web_mgr_8cpp.html#af746297775839f25b039311b3bf9db74",
-"classc___input_artnet.html#a004bc4bc6d01597111e959dc6bb7cd02",
-"classc___input_f_p_p_remote_play_effect.html#aa23717485961fb645d938014af8bf40f",
-"classc___output_common.html#a9b9ccc6ceee5ad7033235824b2d87b43",
-"classc___web_mgr.html#af729e648a154b8a0228c6fe90dba70ab",
-"classfsm___wi_fi__state___boot.html#a29211bf09c3f160062c3b43244b1661e",
-"globals_vars_f.html",
-"structc___output_mgr_1_1_driver_info__t.html#a84137a376de749d21ba0f8cd38758eba"
+"_g_p_i_o___defs___e_s_p32___d1___m_i_n_i___e_t_h_8hpp.html#ab9bf37808f8fcd1cf5c562380d2f45eb",
+"_g_p_i_o___defs___e_s_p32___m_h___e_t___l_i_v_e___mini_kit_8hpp.html#a31c0e7df4948a6a1233547ea876a44f9",
+"_g_p_i_o___defs___e_s_p32___q_u_i_n_l_e_d___q_u_a_d___e_t_h_8hpp.html#ad828b40b5ce0c37516acd57858300a64",
+"_g_p_i_o___defs___e_s_p32___t_w_i_l_i_g_h_t_l_o_r_d___e_t_h_8hpp.html#a639c3538a0fd379beffa18dacae31fb7",
+"_input_d_d_p_8h.html#ad610c50874a341754d728e837fb2c094",
+"_web_mgr_8cpp.html#ad5f3fb1400161c793beaa51883045634",
+"classc___input_alexa.html#af7e02b8a85f9cfdcf08aa30a0898b485",
+"classc___input_f_p_p_remote_play_effect.html#aa0a2b271e77ed90a7db836e2032068e3",
+"classc___output_common.html#a89a48db3e49b37b61b0049d4c411cded",
+"classc___web_mgr.html#ae959e70739b7d2baed8f527572458855",
+"classfsm___wi_fi__state.html#ac8864302765a2c9850f2765ac65e6ad3",
+"globals_vars_c.html",
+"structc___output_mgr_1_1_driver_info__t.html#a40297b034c878f233ff995625fa7f8cd"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
