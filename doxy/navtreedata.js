99bde42d7d0e1f2e9e08836965720bfe17106269
MartinMueller2003 <MartinMueller2003@users.noreply.github.com>
2025-07-23 14:18:34 +0000

    deploy: a35145130ff8201570a79d6f73600359f0e69840

--- doxy/navtreedata.js
+++ doxy/navtreedata.js
@@ -56,19 +56,19 @@ var NAVTREEINDEX =
 "_const_names_8cpp.html",
 "_const_names_8hpp.html#a0a93c30ec2a6fafe061f9abefaf5025b",
 "_e_s_pixel_stick_8h.html#a067b67c856af5452506085e83cdb2b01",
-"_g_p_i_o___defs___e_s_p32___d1___m_i_n_i___e_t_h_8hpp.html#aac8484c1f4cc7cb788c03e1c3c1faade",
-"_g_p_i_o___defs___e_s_p32___m_h___e_t___l_i_v_e___mini_kit_8hpp.html#a14f66e174569445ed297c0d2948c56c3",
-"_g_p_i_o___defs___e_s_p32___q_u_i_n_l_e_d___q_u_a_d___e_t_h_8hpp.html#ad1cdaf2881d19f91216c4a64f925d072",
-"_g_p_i_o___defs___e_s_p32___t_w_i_l_i_g_h_t_l_o_r_d___e_t_h_8hpp.html#a4519f799b3e11a08d0b7377dac008a4e",
-"_input_d_d_p_8h.html#abba4e66dd408809819f6c486964d605c",
-"_sensor_d_s18_b20_8h_source.html",
-"classc___file_mgr.html#afd39c494beddbc229522e53fb4ce56d4",
-"classc___input_f_p_p_remote.html#a8cb165076ce28fc5bc3f16f5cc9ea5f4",
-"classc___network_mgr.html#adb2bfe0b52822c8b9936696d9d03031d",
-"classc___output_uart.html#afe889ae7e67687c4315474ce3aabb82f",
-"classfsm___play_list__state___playing_effect.html#aa33727f0d728e2ddc9239e285e323bd7",
-"globals_defs_n.html",
-"structc___input_effect_engine_1_1d_c_r_g_b.html#a3b8fd353a7a3937773114d2e51154da4"
+"_g_p_i_o___defs___e_s_p32___d1___m_i_n_i___e_t_h_8hpp.html#aa8f8d73c965f04acf2513dea0b5443c5",
+"_g_p_i_o___defs___e_s_p32___m_h___e_t___l_i_v_e___mini_kit_8hpp.html",
+"_g_p_i_o___defs___e_s_p32___q_u_i_n_l_e_d___q_u_a_d___e_t_h_8hpp.html#acd6e6f0240598bf75d38ce85c4b91d92",
+"_g_p_i_o___defs___e_s_p32___t_w_i_l_i_g_h_t_l_o_r_d___e_t_h_8hpp.html#a4372cae3244b7798703bf70bef4dc2a9",
+"_input_d_d_p_8h.html#ab9e017559a744e8946a7c4631bd6440d",
+"_sensor_d_s18_b20_8h.html",
+"classc___file_mgr.html#aee657c03ad56d87d9cb39970d4de4951",
+"classc___input_f_p_p_remote.html#a7b84b8b1119182f7a2c6a72c00d1d1da",
+"classc___network_mgr.html#ad0481bf9efb052bc76c239c6f6b90c54",
+"classc___output_uart.html#afa50de8c9fc4b848160d18576694c4b8a84e7bc8789d38d9f54c35bf95f4555a4",
+"classfsm___play_list__state___playing_effect.html#a19d33fe45d53594db5f58f777f293a1d",
+"globals_defs_j.html",
+"structc___input_effect_engine_1_1d_c_h_s_v.html#a8ba380a9ebbd1f09d5a75c05ae85b6e9"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
